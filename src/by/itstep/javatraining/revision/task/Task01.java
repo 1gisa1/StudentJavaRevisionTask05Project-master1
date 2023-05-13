e4b319fd188df31fbb34cc777b5c620513886c53
1gisa1 <128623341+1gisa1@users.noreply.github.com>
2023-05-13 20:49:08 +0300

    done

--- src/by/itstep/javatraining/revision/task/Task01.java
+++ src/by/itstep/javatraining/revision/task/Task01.java
@@ -24,8 +24,85 @@ package by.itstep.javatraining.revision.task;
  *	[output 4]: 0
  */
 
+import javafx.print.Collation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Task01 {
     public static int task01(int a, int b, int c, int d) {
-        return 0;
+
+        List<Integer> arraylist = Arrays.asList(a, b, c, d);
+        Collections.sort(arraylist);
+
+        int count = 1;
+        for (int i = 0; i < arraylist.size() - 1; i++) {
+            if (arraylist.get(i) == arraylist.get(i + 1)) {
+                count++;
+            }
+        }
+
+        if (count == 1) {
+            count = 0;
+        }
+
+
+        return count;
     }
+
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 7;
+        int c = 3;
+        int d = 7;
+
+        ArrayList arraylist = new ArrayList();
+        arraylist.add(a);
+        arraylist.add(b);
+        arraylist.add(c);
+        arraylist.add(d);
+
+        Collections.sort(arraylist);
+        System.out.println(arraylist);
+
+        int count = 1;
+        for (int i = 0; i < arraylist.size() - 1 ; i++) {
+            if (arraylist.get(i) == arraylist.get(i+1)){
+                count++;
+            }
+        }
+
+        if (count == 1){
+            count = 0;
+        }
+
+        System.out.println(count);
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
